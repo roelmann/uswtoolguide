f7559e80106063c30ed1f551f8f96f24408ed124
Richard Oelmann <oelmann.richard@gmail.com>
2015-09-22 20:45:55 +0100

    Resize footer text

--- includes/footer.php
+++ includes/footer.php
@@ -51,8 +51,7 @@
 		</div><!-- more info -->
 	</div><!-- row -->
 		<div class="well credits">
-		<h3>Adapted by the USW TELT (University of South Wales Technology Enhanced Learning Team) from a development by Richard Oelmann (@Oelmann_Richard) of the original <a href="http://www.cats-pyjamas.net/moodle-tool-guide/ alt="Moodle Tool Guide">Moodle Tool Guide</a> by Joyce Seitzinger (@catspyjamasnz) and the <a href="http://www.somerandomthoughts.com/blog/2012/03/15/a-moodle-2-version-of-the-moodle-tool-guide/" alt="Moodle2 Tool Guide">Moodle 2 Tool Guide</a> by Gavin Henrick (@ghenrick)</h3>
-			<h3>The Tool Selector is based on the concepts and the overview of the <a href="https://dl.dropboxusercontent.com/u/423876/Moodle/Index.html" alt="Tool selector link">Tool Selection Decision Tree</a> by Alison Ruth</h3>
-			<p><img src="images/by-nc-sa.png" alt="Creative Commons licence - by, nc, sa"> As defined in the original, this work is released under a <a href="https://creativecommons.org/licenses/">Creative Commons, with attribution, non-commercial, share-alike licence.</a></p>
+		    <p>Adapted by the USW TELT (University of South Wales Technology Enhanced Learning Team) from a development by Richard Oelmann (@Oelmann_Richard) of the original <a href="http://www.cats-pyjamas.net/moodle-tool-guide/ alt="Moodle Tool Guide">Moodle Tool Guide</a> by Joyce Seitzinger (@catspyjamasnz) and the <a href="http://www.somerandomthoughts.com/blog/2012/03/15/a-moodle-2-version-of-the-moodle-tool-guide/" alt="Moodle2 Tool Guide">Moodle 2 Tool Guide</a> by Gavin Henrick (@ghenrick)</p>
+		    <p>The Tool Selector is based on the concepts and the overview of the <a href="https://dl.dropboxusercontent.com/u/423876/Moodle/Index.html" alt="Tool selector link">Tool Selection Decision Tree</a> by Alison Ruth</p>			<p><img src="images/by-nc-sa.png" alt="Creative Commons licence - by, nc, sa"> As defined in the original, this work is released under a <a href="https://creativecommons.org/licenses/">Creative Commons, with attribution, non-commercial, share-alike licence.</a></p>
 		</div>
 </div><!-- footer -->
